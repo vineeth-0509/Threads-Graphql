f83f37cd055bdac5ecb5f37f945a78b669e0d5f0
Vineeth-0509 <vineeththungani@gmail.com>
2025-05-04 16:07:56 +0530

    added prisma and created a mutation for the create user

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,63 @@ import { expressMiddleware } from "@apollo/server/express4";
 
 import cors from "cors";
 //import bodyParser from "body-parser";
-import express, {Express} from "express";
+import express, { Express } from "express";
+import { prismaClient } from "./lib/db";
 const typeDefs = `#graphql
 type Query{
   hello: String
   say(name: String): String
 }
+type Mutation {
+  createUser(firstName: String!, lastName: String!, email:String!, password:String!): Boolean
+}
 `;
 const resolvers = {
   Query: {
     hello: () => "Hello world!",
-    say:(_: any,{name}:{name:string}) =>`Hey ${name} how are you?`
+    say: (_: any, { name }: { name: string }) => `Hey ${name} how are you?`,
+  },
+  Mutation: {
+    createUser: async (
+      _: any,
+      {
+        firstName,
+        lastName,
+        email,
+        password,
+      }: {
+        firstName: string;
+        lastName: string;
+        email: string;
+        password: string;
+      }
+    ) => {
+      await prismaClient.user.create({
+        data: {
+          email,
+          firstName,
+          lastName,
+          password,
+          salt: "random_salt",
+        },
+      });
+      return true;
+    },
   },
 };
 async function startServer() {
-  const app:Express = express();
+  const app: Express = express();
   app.use(express.json());
-  const server = new ApolloServer({typeDefs, resolvers})
+  const server = new ApolloServer({ typeDefs, resolvers });
   await server.start();
 
-  
-  app.use("/graphql",cors<cors.CorsRequest>(), express.json(), expressMiddleware(server),(req,res)=>{
-
-  });
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    express.json(),
+    expressMiddleware(server),
+    (req, res) => {}
+  );
   app.get("/", (req, res) => {
     res.json({ message: "server is up and running" });
   });
